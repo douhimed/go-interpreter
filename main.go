01fa1f56e6e2b21b653c4e0afca5e8014f527a11
mdouhi <mdouhi@sqli.com>
2024-09-17 16:50:18 +0200

    add repl and main to start writing commands

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"go-interpreter/repl"
+	"os"
+	"os/user"
+)
+
 /**
 *
 * 1. The lexer
@@ -9,3 +16,15 @@ package main
 * 5. The evaluator
 *
  */
+
+func main() {
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s into Monkey prog language! \n", user.Username)
+	fmt.Printf("Type a commands \n\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
