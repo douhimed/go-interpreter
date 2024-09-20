e1b58d201fe0a5eee4050254a7e9b0792f6e168d
mdouhi <mdouhi@sqli.com>
2024-09-20 17:53:40 +0200

    first dev ast

---
+++ ast/ast.go
@@ -0,0 +1,27 @@
+package ast
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	StatementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statement []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statement) > 0 {
+		return p.Statement[0].TokenLiteral()
+	}
+
+	return ""
+}
