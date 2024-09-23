83e22f555940e70866b1e2ff49a89f698dfbb9f7
mdouhi <mdouhi@sqli.com>
2024-09-23 19:16:03 +0200

    parsing return statement

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -99,3 +99,33 @@ func checkParseErrors(t *testing.T, p *Parser) {
 
 	t.FailNow()
 }
+
+func TestReturnStatement(t *testing.T) {
+	input := `
+	return 5;
+	return 10;
+	return 993322;
+	`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParseErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does noet contains 3 statements, got=%d", len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.returnStatemtn, got=%T", stmt)
+			continue
+		}
+
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TOkenLiteral not 'return', got %q", returnStmt.TokenLiteral())
+		}
+	}
+}
