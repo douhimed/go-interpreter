a7fc4b5c782bfef2bec2db237bc461edc4c2798d
mdouhi <mdouhi@sqli.com>
2024-10-03 17:16:43 +0200

    add String() for program components

--- parser/parser.go
+++ parser/parser.go
@@ -86,8 +86,7 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
-	// TODO: We're skipping the expressions until we
-	// encounter a semicolon
+
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
